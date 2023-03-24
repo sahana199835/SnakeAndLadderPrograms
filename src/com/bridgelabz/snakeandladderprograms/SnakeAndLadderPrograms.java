2556b3d7bd5dd564574f93c2acb0f51003e324fb
sahana199835 <tnsahana1@gmail.com>
2023-03-24 13:55:35 +0530

    add:.gitignore and README.md files are added

---
+++ src/com/bridgelabz/snakeandladderprograms/SnakeAndLadderPrograms.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.snakeandladderprograms;
+
+public class SnakeAndLadderPrograms {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to snake and ladder programs");
+
+	}
+
+}
