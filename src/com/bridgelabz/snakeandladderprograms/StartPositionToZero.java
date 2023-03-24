26221f2f4ae20a2c6050c30b9822088d10d6e6be
sahana199835 <tnsahana1@gmail.com>
2023-03-24 13:59:00 +0530

    add-check the start position to zero

---
+++ src/com/bridgelabz/snakeandladderprograms/StartPositionToZero.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.snakeandladderprograms;
+
+public class StartPositionToZero {
+
+	public static void main(String[] args) {
+		
+		int player_Position=0;
+	    System.out.println("Player's starting position is: " +player_Position );
+}
+
+	}
+
+
